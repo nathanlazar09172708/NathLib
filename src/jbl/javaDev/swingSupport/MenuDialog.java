7cbfaaf6fcf6a10b357c0b1ea83836d815b76bf0
HP <HP@DESKTOP-I8V8GMV>
2020-06-14 20:55:57 +0800

    MenuDialog imroved Able to set multiple MenuHovers in one MenuDialog removed regMenuHover to have relationship between multiple MenuHovers improved DIALOG_APPEARANCE_LEFT_SIDE behavior in animateExit

--- src/jbl/javaDev/swingSupport/MenuDialog.java
+++ src/jbl/javaDev/swingSupport/MenuDialog.java
@@ -57,6 +57,8 @@ public class MenuDialog extends JDialog{
                  *      the animation of MenuDialog still runs
                  *
 		 */
+                
+           
         Toolkit.getDefaultToolkit().addAWTEventListener(
                 
             new AWTEventListener(){
@@ -69,21 +71,39 @@ public class MenuDialog extends JDialog{
                         if(!(event.getSource()==MenuDialog.this.menuHover)){
                             
                             if(event.getID()==MouseEvent.MOUSE_EXITED){
-                                
+                               
                                 try{
                                     
                                     if(MenuDialog.this.getOpacity()==1.0f){
-                                        
+                                         
                                         if(!isAnimationRunning()){
                                             
-                                            if(!(MouseInfo.getPointerInfo().getLocation().x>MenuDialog.this.getLocation().x
-                                                &&MouseInfo.getPointerInfo().getLocation().x<MenuDialog.this.getLocation().x+MenuDialog.this.getWidth()
-                                                &&MouseInfo.getPointerInfo().getLocation().y>MenuDialog.this.getLocation().y
-                                                &&MouseInfo.getPointerInfo().getLocation().y<MenuDialog.this.getLocation().y+MenuDialog.this.getHeight())){
+                                            if(getDialogAppearance()==MenuDialog.DIALOG_APPEARANCE_DOWN_SIDE_LEFT_ALIGN||getDialogAppearance()==MenuDialog.DIALOG_APPEARANCE_DOWN_SIDE_RIGHT_ALIGN){
                                                 
-                                                animateExit();
-                                            
+                                                if(!(MouseInfo.getPointerInfo().getLocation().x>MenuDialog.this.getLocation().x
+                                                    &&MouseInfo.getPointerInfo().getLocation().x<MenuDialog.this.getLocation().x+MenuDialog.this.getWidth()
+                                                    &&MouseInfo.getPointerInfo().getLocation().y>MenuDialog.this.getLocation().y
+                                                    &&MouseInfo.getPointerInfo().getLocation().y<MenuDialog.this.getLocation().y+MenuDialog.this.getHeight())){
+                                                
+                                                    animateExit();
+                                                
+                                                }
+                                                
                                             }
+                                            else if(getDialogAppearance()==MenuDialog.DIALOG_APPEARANCE_LEFT_SIDE){
+                                                
+                                                if(MouseInfo.getPointerInfo().getLocation().x<MenuDialog.this.getLocation().x
+                                                    ||MouseInfo.getPointerInfo().getLocation().y<MenuDialog.this.getLocation().y
+                                                    ||(MouseInfo.getPointerInfo().getLocation().x>MenuDialog.this.getLocation().x+MenuDialog.this.getWidth()
+                                                        &&MouseInfo.getPointerInfo().getLocation().y>MenuDialog.this.menuHover.getLocationOnScreen().y+MenuDialog.this.menuHover.getHeight())
+                                                    ||MouseInfo.getPointerInfo().getLocation().y>MenuDialog.this.getLocation().y+MenuDialog.this.getHeight()){
+                                                    
+                                                    animateExit();
+                                                    
+                                                }
+                                                
+                                            }
+                                            
                                         
                                         }
                                     
@@ -107,16 +127,6 @@ public class MenuDialog extends JDialog{
             
             }
         ,AWTEvent.MOUSE_MOTION_EVENT_MASK+AWTEvent.MOUSE_EVENT_MASK);
-    
-    }
-	
-    /**
-     * The regMenuHover method to bind menuHover to dialog
-     * @param menuHover binds with dialog
-     */
-    protected void regMenuHover(MenuHover menuHover){
-        
-        this.menuHover=menuHover;
     
     }
     
@@ -160,7 +170,7 @@ public class MenuDialog extends JDialog{
     
     }
 	
-    private void animateExit(){
+    protected void animateExit(){
         
         this.setAnimationRunning(true);
         
@@ -177,6 +187,7 @@ public class MenuDialog extends JDialog{
         }
 
         fadeEffect.start();
+        
     
     }
         
@@ -185,8 +196,9 @@ public class MenuDialog extends JDialog{
      * @param x initial x location
      * @param y initial y location
      */
-    protected void animate(int x,int y){
+    protected void animate(int x,int y,MenuHover menuHover){
         
+        this.menuHover=menuHover;
         this.setLocation(x,y);
         this.setOpacity(0.0f);
         this.setVisible(true);
@@ -329,10 +341,12 @@ public class MenuDialog extends JDialog{
 		MenuDialog.this.setVisible(false);
 		
                 if(MenuDialog.this.getDialogAppearance()==MenuDialog.DIALOG_APPEARANCE_LEFT_SIDE||MenuDialog.this.getDialogAppearance()==MenuDialog.DIALOG_APPEARANCE_RIGHT_SIDE){
-                
+                    
                     MenuDialog.this.setAnimationRunning(false);
-                
+                       
+                    
                 }
+                
                 
                 fadeEffect.stop();
                 
